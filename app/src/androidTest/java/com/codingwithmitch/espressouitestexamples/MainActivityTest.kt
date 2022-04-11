930b94d7e2bb490fc5c7c44df79c6ce0c9b62d2c
prasannakumar2495 <prasannakumar2461995@gmail.com>
2022-04-11 12:27:29 +0530

    minor changes

--- app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/MainActivityTest.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/MainActivityTest.kt
@@ -8,12 +8,13 @@ import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.hamcrest.core.IsNot.not
 import org.junit.Test
 
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
-class MainActivityTest{
+class MainActivityTest {
 
 
     @Test
